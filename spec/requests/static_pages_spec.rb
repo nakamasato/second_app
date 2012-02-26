bb16b9e6833b2ea6a652c79d4bed553a5d5b47e6
nakamasato <utjxs.ntn@hotmail.co.jp>
2012-02-26 16:01:32 +0900

    abbreviate the expression in rspec file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,34 +3,19 @@ require 'spec_helper'
 describe "StaticPages" do
   let(:base_title){"Ruby on Rails Tutorial Sample App"}
   describe "GET /static_pages" do
-    it "should have the content 'Home'" do
-      visit root_path
-      page.should have_selector('h1', :text => "Sample App")
-    end
-    it "should have the right title" do 
-      visit root_path
-      page.should have_selector('title', :text =>"#{base_title} | Home")
-    end
+    before { visit root_path }
+    it { page.should have_selector('h1', :text => "Sample App") }
+    it { page.should have_selector('title', :text =>"#{base_title} | Home") }
   end
   describe "help page" do
-    it "should have the content 'help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
-    it "should have the right title" do 
-      visit help_path
-      page.should have_selector('title', :text =>"Ruby on Rails Tutorial Sample App | Help")
-    end
+    before { visit help_path }
+    it { page.should have_selector('h1', :text => "Help") }
+    it { page.should have_selector('title', :text =>"Ruby on Rails Tutorial Sample App | Help") }
   end
   describe "About page" do
-    it "should have the content 'about'" do
-      visit about_path
-      page.should have_content('About')
-    end
-    it "should have the right title" do 
-      visit about_path
-      page.should have_selector('title', :text =>"Ruby on Rails Tutorial Sample App | About")
-    end
+    before { visit about_path }
+    it { page.should have_content('About') }
+    it { page.should have_selector('title', :text =>"Ruby on Rails Tutorial Sample App | About") }
   end
   describe "Contact page" do
     it "should have the content 'Contact'" do
